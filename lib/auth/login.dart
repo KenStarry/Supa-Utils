967b0563c6cced3dc7b6ceb639392c0eef6b22d1
KenStarry <starrycodes@gmail.com>
2024-11-04 11:42:56 +0300

    Supabase Sign In with email and password

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -3,6 +3,19 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 mixin SupabaseLogin {
   final supabase = Supabase.instance.client;
 
+  /// Sign In User with Email and Password
+  Future<AuthResponse> signInWithEmailAndPassword(
+      {required String email, required String password}) async {
+    try {
+      final AuthResponse response =
+      await supabase.auth.signInWithPassword(email: email, password: password);
 
+      return response;
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+
+  /// Google Sign In
 }
 
