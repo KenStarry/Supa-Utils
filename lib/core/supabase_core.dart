9f92a4f75ca3e718200cf5cf4a11ad03ab53abee
KenStarry <starrycodes@gmail.com>
2024-11-04 15:00:30 +0300

    Added Supabase Fetch to Core

--- lib/core/supabase_core.dart
+++ lib/core/supabase_core.dart
@@ -1,10 +1,9 @@
 part of '../supa_utils.dart';
 
-/// Initialize Supabase
-Future<void> initializeSupabase(
-    {required String url, required String anonKey}) async =>
-    await Supabase.initialize(url: url, anonKey: anonKey);
-
-class SupabaseCore with SupabaseLogin, SupabaseSignUp, SupabaseAdd {
-
+class SupabaseCore
+    with SupabaseLogin, SupabaseSignUp, SupabaseAdd, SupabaseFetch {
+  /// Initialize Supabase
+  Future<void> initializeSupabase(
+          {required String url, required String anonKey}) async =>
+      await Supabase.initialize(url: url, anonKey: anonKey);
 }
