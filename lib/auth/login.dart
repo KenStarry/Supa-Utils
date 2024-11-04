9c05a4d85dfa666f3ca2646446d6f72f34960644
KenStarry <starrycodes@gmail.com>
2024-11-04 11:58:07 +0300

    Google Sign In Response model and updates

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -1,4 +1,7 @@
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'models/google_sign_in_response_model.dart';
 
 mixin SupabaseLogin {
   final supabase = Supabase.instance.client;
@@ -7,8 +10,8 @@ mixin SupabaseLogin {
   Future<AuthResponse> signInWithEmailAndPassword(
       {required String email, required String password}) async {
     try {
-      final AuthResponse response =
-      await supabase.auth.signInWithPassword(email: email, password: password);
+      final AuthResponse response = await supabase.auth
+          .signInWithPassword(email: email, password: password);
 
       return response;
     } catch (error) {
@@ -17,5 +20,42 @@ mixin SupabaseLogin {
   }
 
   /// Google Sign In
-}
+  Future<GoogleSignInResponseModel> signInWithGoogle(
+      {String? webClientID, String? iosClientID}) async {
+    try {
+      final GoogleSignIn googleSignIn =
+          GoogleSignIn(serverClientId: webClientID, clientId: iosClientID);
 
+      final googleUser = await googleSignIn.signIn();
+
+      if (googleUser != null) {
+        final googleAuth = await googleUser.authentication;
+        final accessToken = googleAuth.accessToken;
+        final idToken = googleAuth.idToken;
+
+        if (accessToken == null) {
+          throw 'No Access Token found';
+        }
+
+        if (idToken == null) {
+          throw 'No ID Token Token found';
+        }
+
+        final AuthResponse response = await supabase.auth.signInWithIdToken(
+            provider: OAuthProvider.google,
+            idToken: idToken,
+            accessToken: accessToken);
+
+        return GoogleSignInResponseModel(
+            response: response,
+            auth: googleAuth,
+            accessToken: accessToken,
+            idToken: idToken);
+      } else {
+        throw 'Google User is null';
+      }
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+}
